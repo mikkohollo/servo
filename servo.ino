66e4b417b612da5ded5f179aef1f023aa9035072
Mikko Hollo <mikko.hollo@edu.turkuamk.fi>
2015-05-07 12:36:44 +0300

    no message

--- servo.ino
+++ servo.ino
@@ -1,11 +1,11 @@
 #include <Servo.h>
 
 Servo myservo;  // create servo object to control a servo
-// a maximum of eight servo objects can be created
 
 int velocity = 1500;
 const int counter = 67;
-int acceleration_clockwise[counter] = {1,2,3,4,5,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,5,4,3,2,1,0,0,0,0,0,0,
+int acceleration_clockwise[counter] = {
+  1,2,3,4,5,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,5,4,3,2,1,0,0,0,0,0,0,
   -1,-2,-3,-4,-5,-6,-6,-6,-6,-6,-6,-6,-6,-6,-6,-6,-6,-6,-6,-6,-6,-6,-6,-6,-6,-5,-4,-3,-2,-1,0};
 void setup()
 {
@@ -20,17 +20,18 @@ void loop()
     velocity = velocity + acceleration_clockwise[i];
     myservo.writeMicroseconds(velocity);
     Serial.println(velocity);
-    delay(500);
+    delay(200);
   } 
   delay(500);                                         
   for(int i = 0; i < counter; i++){                  //Counterclockwise
     velocity = velocity - acceleration_clockwise[i];
     myservo.writeMicroseconds(velocity);
     Serial.println(velocity);
-    delay(500);
+    delay(200);
   }
   delay(500);
 }
+
 
 
 
